829f432b344243ca2607d9935dfd64e9cb8aa833
geniusvlg <geniusvlg@yahoo.com.vn>
2017-05-16 08:07:52 +0700

    Extract images for pop-up

--- js/game.js
+++ js/game.js
@@ -1,115 +1,44 @@
-loadCallback = function () {
-  $h_header = $('#header').outerHeight(true);
-  $h_footer = $('#footer').outerHeight(true);
-  $h_window = $(window).outerHeight(true);
-  $h_content = $h_window - ($h_header + $h_footer);
-  $width = $(document).outerWidth(true);
-
-  $('#content').css('height', $h_content).show();
-  $('.turn').css('bottom', $h_footer + 10);
-  $('.btn-share-home').css('bottom', $h_footer + 10);
-  $h_content = $('#content').outerHeight(true);
-  var a_canvas;
-  if ($width > $h_content + 20) {
-    a_canvas = $h_content - 20;
-    $('#canvas').css('margin-top', "10px");
-  }
-  else {
-    a_canvas = $width - 40;
-    $mt = ($h_content - a_canvas) / 2;
-    $('#canvas').css('margin-top', $mt + "px");
-  }
-
-  $('#canvas').css('width', a_canvas + "px");
-  $('#canvas').css('height', a_canvas + "px");
-};
-
-isPlay = false;
 function rotateImg() {
 
-    if (isPlay == false) {
-    $('#mask').fadeIn(200);
-//    var k = Math.floor((Math.random() * 11) + 0);
-//    var round = Math.floor((Math.random() * 8) + 6);
-    isPlay = true;
-    setTimeout(function () {
-      play();
-    }, 200);
-  }
+  $('#mask').fadeIn(200);
+  setTimeout(function () {
+    play();
+  }, 200);
 }
 
+
+
 function play() {
-  var round = 14;
-  var ang = round * 360;
+  // Get a random reward
+  var reward =  reward_list[Math.floor(Math.random() * reward_list.length)];
+  var obj = JSON.parse(data);
+  var round = 12;
+  console.log(obj[reward])
+  var ang = round * 360 + parseInt(obj[reward][0].angle);
   var token = $('#my_token').val();
 
   $('#wheel').rotate({
     angle: 0,
     animateTo: ang,
-    duration: 3000,
-    callback: function() {
-      console.log("finish");
-      showPopup('popup-error');
+    duration: 7000,
+    callback: function(){
+      //showPopup(obj[reward][1].image, obj[reward][2].content)
     }
   });
-
-
-  var ok = ({
-    url: $('#url_rotate').val(),
-    dataType: 'json',
-    data: {token: token},
-    success: function (data) {
-      if (data.result) {
-        $('#my_token').val(data.token);
-        ang = data.angle + ang;
-        $('#wheel').rotate({
-          animateTo: ang,
-          duration: 15000,
-//            easing: $.easing.easeInOutQuart,
-          callback: function () {
-            token = $('#my_token').val();
-//            var url_reward = $('#url_reward').val();
-//            url_reward = url_reward + "?win=" + data.win + "&token=" + token;
-//            $.get(url_reward);
-
-            if(data.type == "SHARE"){
-              data.note += "<br><br><img style='cursor:pointer' onclick='shareFB(1)' src='/wap/images/btn-share.png' />";
-            }
-            $('#popup-result div.bold').html(data.message);
-            $('#popup-result div.description').html(data.note);
-
-            $('#mask').hide();
-            showPopup('popup-result');
-            if (data.turn <= 0) {
-              $('#canvas button').addClass('inactive');
-            }
-            else {
-              $('#canvas button').css('opacity', '1');
-              isPlay = false;
-            }
-            $('#total-turn').html(data.turn);
-          }
-        });
-      }
-      else {
-        stop();
-        $('#popup-result div.description').html(data.message);
-        showPopup('popup-error');
-        setTimeout(function () {
-          location.reload();
-        }, 1500);
-      }
-    },
-    error: function () {
-      stop();
-      showPopup('popup-error');
-      setTimeout(function () {
-        //location.reload();
-      }, 1500);
-    },
-    timeout: 10000
-  });
 }
+
+function showPopup(image_url, content) {
+  console.log("Show popup")
+  var $firework = $(".firework");
+  $firework.css("display","block");
+
+  var $popup = $(".popup");
+  $popup.css("display","block");
+
+   console.log("finish");
+  $('#reward-img').attr('src', image_url)
+}
+
 
 function stop() {
   $('#canvas').hide();
